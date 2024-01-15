0323a50b1b0fbd58ec0bbccae0dce053d45914f2
minakshi nehe <minakshinehe2211@gmail.com>
2024-01-15 23:15:39 +0530

    created Login API structure

--- BackEnd/CityListAPI/src/main/java/com/Zomato/CityListAPI/DTO/ResponseDTO.java
+++ BackEnd/CityListAPI/src/main/java/com/Zomato/CityListAPI/DTO/ResponseDTO.java
@@ -5,7 +5,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j  @Getter @Setter @NoArgsConstructor
+//@Slf4j  @Getter @Setter @NoArgsConstructor
+
 public class ResponseDTO {
 	
 	int statusCode;
---
+++ BackEnd/LoginAPI/src/test/java/com/Zomato/LoginAPI/LoginApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Zomato.LoginAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
